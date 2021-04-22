98a7892ccba6756d9ea8153e9d20c69669a2f930
Jakiboy <j.sinnaour.official@gmail.com>
2021-04-22 17:31:41 +0000

    Update

--- src/Kernel/MiddlewareException.php
+++ src/Kernel/MiddlewareException.php
@@ -5,12 +5,12 @@
  * @subpackage: Exceptions Component
  * @version   : 1.0.0
  * @category  : PHP framework
- * @copyright : (c) JIHAD SINNAOUR <mail@jihadsinnaour.com>
+ * @copyright : (c) 2017 - 2021 JIHAD SINNAOUR <mail@jihadsinnaour.com>
  * @link      : https://www.floatphp.com
  * @license   : MIT License
  */
 
-namespace floatPHP\Exceptions\Kernel;
+namespace FloatPHP\Exceptions\Kernel;
 
 use \Exception;
 
